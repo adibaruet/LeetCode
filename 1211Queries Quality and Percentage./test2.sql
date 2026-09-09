119af9f30f05641d1e0d8b8710256df65d18f9ea
Humaira Tasnim Adiba <123799736+adibaruet@users.noreply.github.com>
2026-09-09 18:52:20 +0600

    Create test2.sql

---
+++ 1211Queries Quality and Percentage./test2.sql
@@ -0,0 +1,9 @@
+SELECT
+    query_name,
+    ROUND(AVG(rating * 1.0 / position), 2) AS quality,
+    ROUND(
+        COUNT(*) FILTER (WHERE rating < 3) * 100.0 / COUNT(*),
+        2
+    ) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
