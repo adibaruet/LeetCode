d2a08fe44de0e7c4f9b4091f3c2fbcd4c7add1c3
Humaira Tasnim Adiba <123799736+adibaruet@users.noreply.github.com>
2026-09-11 16:05:26 +0600

    Create test2.sql

---
+++ 1174. Immediate Food Delivery II/test2.sql
@@ -0,0 +1,20 @@
+WITH first_orders AS (
+    SELECT *,
+           ROW_NUMBER() OVER (
+               PARTITION BY customer_id
+               ORDER BY order_date
+           ) AS rn
+    FROM Delivery
+)
+SELECT
+    ROUND(
+        100.0 * AVG(
+            CASE
+                WHEN order_date = customer_pref_delivery_date THEN 1
+                ELSE 0
+            END
+        ),
+        2
+    ) AS immediate_percentage
+FROM first_orders
+WHERE rn = 1;
