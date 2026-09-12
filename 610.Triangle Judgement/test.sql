165e1bb358f1efcde4ca225bf6ff7e8062b5f247
Humaira Tasnim Adiba <123799736+adibaruet@users.noreply.github.com>
2026-09-12 21:27:44 +0600

    Add SQL query to determine triangle validity

--- 610.Triangle Judgement/test.sql
+++ 610.Triangle Judgement/test.sql
@@ -1,1 +1,12 @@
-a
+SELECT 
+    x,
+    y,
+    z,
+    CASE
+        WHEN x + y > z
+         AND x + z > y
+         AND y + z > x
+        THEN 'Yes'
+        ELSE 'No'
+    END AS triangle
+FROM Triangle;
